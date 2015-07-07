8aa024bbf4c22dcfa70d02ae9215b5abbc7823fc
Frank Guo <pkushiqiang@gmail.com>
2015-07-07 17:36:42 -0500

    add some code

---
+++ src/mynote/mynote/algorithm/combination/RepeatableCombination.java
@@ -0,0 +1,52 @@
+package mynote.algorithm.combination;
+
+import java.util.*;
+
+public class RepeatableCombination {
+
+	public static List<List<Integer>> repeatableCombination(int[] A, int k) {
+		List<List<Integer>> res = new ArrayList<>();
+		if (A == null || A.length == 0 || k == 0)
+			return res;
+
+		List<Integer> list = new ArrayList<>();
+		helper(res, list, A, 0, 0, k);
+		return res;
+	}
+
+	private static void helper(List<List<Integer>> res, List<Integer> list,
+			int[] A, int s, int n, int k) {
+		if (n == k) {
+			res.add(new ArrayList(list));
+			return;
+		}
+
+		for (int i = s; i < A.length; i++) {
+			list.add(A[i]);
+			helper(res, list, A, i, n + 1, k);
+			list.remove(list.size() - 1);
+		}
+	}
+	
+	public static void printRes(int[] A, int k) {
+		List<List<Integer>> res = repeatableCombination(A, k);
+		for (List<Integer> list : res) {
+			for (int i : list) {
+				System.out.print(i + "  ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] A = { 3, 5, 7 };
+		for (int k=0; k<7; k++) {
+			System.out.println("\n\n------------" + k + "------------");
+			printRes(  A,   k) ;
+			
+		}
+		
+	}
+
+}
--- src/mynote/mynote/linkedlist/ListNode.java
+++ src/mynote/mynote/linkedlist/ListNode.java
@@ -2,10 +2,10 @@ package mynote.linkedlist;
 import java.util.*;
 public class ListNode {
 
-	int val;
-	ListNode next;
+	public int val;
+	public ListNode next;
 
-	ListNode(int x) {
+	public ListNode(int x) {
 		val = x;
 	}
 	
